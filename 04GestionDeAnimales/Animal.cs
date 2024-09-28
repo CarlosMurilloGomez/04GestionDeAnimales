52cdabc70235eec2c8efe0b97468a08e2309d6fd
CarlosMurilloGomez <charliemurillo2001@gmail.com>
2024-09-28 16:22:59 +0200

    Añadir clase Animal

---
+++ 04GestionDeAnimales/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04GestionDeAnimales
+{
+    abstract internal class Animal
+    {
+        abstract public String Nombre { get; set; }
+        abstract public Edad Edad { get; set; }
+
+        abstract public void emitirSonido();
+
+    }
+}
